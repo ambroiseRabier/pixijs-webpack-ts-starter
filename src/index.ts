26a1ef91393f3286f7f6f093fe83064a7792a48e
Ambroise Rabier <rabier.ambroise@outlook.fr>
2022-03-03 16:07:55 +0100

    - Adding Esc for closing app, and F11 for fullscreen. - Adding bunny image and changing background to white.

--- src/index.ts
+++ src/index.ts
@@ -5,13 +5,14 @@ import './index.html';
 import spaceshipPng from './spaceship.png';
 
 import {Application, Sprite} from 'pixi.js';
-
+import {appWindow} from '@tauri-apps/api/window';
 
 
 const app = new Application({
   // You should also update tauri.config.json is you change width or height.
   width: 800,
   height: 600,
+  backgroundColor: 0xEEEEEE // Put 0x000000 for black
 });
 
 document.body.appendChild(app.view);
@@ -34,4 +35,15 @@ app.stage.addChild(player);
 app.ticker.add(() => {
   // Rotate player clockwise
   player.rotation += 0.02 * app.ticker.deltaTime;
+});
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Esc' || e.key === 'Escape') {
+    appWindow.close();
+  }
+  if (e.key === 'F11') {
+    appWindow.isFullscreen().then((e) => {
+      appWindow.setFullscreen(!e);
+    });
+  }
 });
